1e72df17c73e69b0f6f9ecd2fac733435fb06466
ShellyC6 <57796687+ShellyC6@users.noreply.github.com>
2023-01-28 18:49:43 +0100

    Etape 3 : Modifier les couleurs

---
+++ open_food_facts_app/lib/appColors.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class AppColors{
+  Color blue = const Color(0xFF080040);
+  Color blueDark = const Color(0xFF050027);
+  Color yellow = const Color(0xFFFBAF02);
+  Color blueLight = const Color(0xFF41C7DF);
+  Color gray1 = const Color(0xFFF6F6F8);
+  Color gray2 = const Color(0xFFB8BBC6);
+  Color gray3 = const Color(0xFF6A6A6A);
+  Color white = const Color(0xFFFFFFFF);
+  Color black = const Color(0xFF000000);
+  Color nutrientLevelLow = const Color(0xFF63993F);
+  Color nutrientKevelModerate = const Color(0xFF997B3F);
+  Color nutrientLevelHigh = const Color(0xFF993F3F);
+  Color nutriscoreA = const Color(0xFF008b4c);
+  Color nutriscoreB = const Color(0xFF80c142);
+  Color nutriscoreC = const Color(0xFFfeca0b);
+  Color nutriscoreD = const Color(0xFFf58220);
+  Color nutriscoreE = const Color(0xFFef3e22);
+  Color ecoScoreA = const Color(0xFF1E8F4E);
+  Color ecoScoreB = const Color(0xFF2ECC71);
+  Color ecoScoreC = const Color(0xFFFFC900);
+  Color ecoScoreD = const Color(0xFFEF7E1A);
+  Color ecoScoreE = const Color(0xFFE62D19);
+}
