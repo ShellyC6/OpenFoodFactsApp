2a81e3ea3f06ea2f97aec05ebb006796a8beaa90
ShellyC6 <57796687+ShellyC6@users.noreply.github.com>
2023-02-15 09:25:06 +0100

    Etape 5 : Utiliser les couleurs

--- open_food_facts_app/lib/appColors.dart
+++ open_food_facts_app/lib/appColors.dart
@@ -1,26 +1,26 @@
 import 'package:flutter/material.dart';
 
 class AppColors{
-  Color blue = const Color(0xFF080040);
-  Color blueDark = const Color(0xFF050027);
-  Color yellow = const Color(0xFFFBAF02);
-  Color blueLight = const Color(0xFF41C7DF);
-  Color gray1 = const Color(0xFFF6F6F8);
-  Color gray2 = const Color(0xFFB8BBC6);
-  Color gray3 = const Color(0xFF6A6A6A);
-  Color white = const Color(0xFFFFFFFF);
-  Color black = const Color(0xFF000000);
-  Color nutrientLevelLow = const Color(0xFF63993F);
-  Color nutrientKevelModerate = const Color(0xFF997B3F);
-  Color nutrientLevelHigh = const Color(0xFF993F3F);
-  Color nutriscoreA = const Color(0xFF008b4c);
-  Color nutriscoreB = const Color(0xFF80c142);
-  Color nutriscoreC = const Color(0xFFfeca0b);
-  Color nutriscoreD = const Color(0xFFf58220);
-  Color nutriscoreE = const Color(0xFFef3e22);
-  Color ecoScoreA = const Color(0xFF1E8F4E);
-  Color ecoScoreB = const Color(0xFF2ECC71);
-  Color ecoScoreC = const Color(0xFFFFC900);
-  Color ecoScoreD = const Color(0xFFEF7E1A);
-  Color ecoScoreE = const Color(0xFFE62D19);
+  static const Color blue = Color(0xFF080040);
+  static const Color blueDark = Color(0xFF050027);
+  static const Color yellow = Color(0xFFFBAF02);
+  static const Color blueLight = Color(0xFF41C7DF);
+  static const Color gray1 = Color(0xFFF6F6F8);
+  static const Color gray2 = Color(0xFFB8BBC6);
+  static const Color gray3 = Color(0xFF6A6A6A);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color black = Color(0xFF000000);
+  static const Color nutrientLevelLow = Color(0xFF63993F);
+  static const Color nutrientKevelModerate = Color(0xFF997B3F);
+  static const Color nutrientLevelHigh = Color(0xFF993F3F);
+  static const Color nutriscoreA = Color(0xFF008b4c);
+  static const Color nutriscoreB = Color(0xFF80c142);
+  static const Color nutriscoreC = Color(0xFFfeca0b);
+  static const Color nutriscoreD = Color(0xFFf58220);
+  static const Color nutriscoreE = Color(0xFFef3e22);
+  static const Color ecoScoreA = Color(0xFF1E8F4E);
+  static const Color ecoScoreB = Color(0xFF2ECC71);
+  static const Color ecoScoreC = Color(0xFFFFC900);
+  static const Color ecoScoreD = Color(0xFFEF7E1A);
+  static const Color ecoScoreE = Color(0xFFE62D19);
 }
